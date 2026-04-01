3cbc672d3548a760acd9d3237d8ab1370fc65f95
Biloliddin Salimov <bilol09876@gmail.com>
2026-04-01 11:44:54 +0500

    change logo and fix it responsive

--- apps/ecommerce/src/components/Navbar.tsx
+++ apps/ecommerce/src/components/Navbar.tsx
@@ -7,6 +7,9 @@ import Link from 'next/link';
 import { cn } from '@/lib/utils';
 import { useCartStore } from '@/store/useCartStore';
 import { useUIStore } from '@/store/useUIStore';
+import Logo from "../../public/svg/Maryam_logo.svg"
+import LogoDark from "../../public/svg/Maryam_logo_darc.svg"
+import Image from 'next/image';
 
 export const Navbar = () => {
   const { t, i18n } = useTranslation();
@@ -28,20 +31,31 @@ export const Navbar = () => {
 
   return (
     <nav className={cn(
-      'fixed top-0 left-0 w-full z-50 transition-all duration-500 py-6 px-8 flex justify-between items-center',
-      isScrolled ? 'bg-brand-cream/90 backdrop-blur-md py-4 shadow-sm' : 'bg-transparent',
+      'fixed top-0 left-0 w-full z-50 transition-all duration-500 py-10 px-8 flex justify-between items-center',
+      // Bu yerda text rangini boshqaramiz:
+      // Scroll bo'lmaganda text-white, scroll bo'lganda text-black (yoki brand-black)
+      isScrolled 
+        ? 'bg-brand-cream/90 backdrop-blur-md py-8 shadow-sm text-black' 
+        : 'bg-transparent text-white',
     )}>
       <div className="hidden lg:flex gap-8 items-center">
-        <Link href="/category/clothing" className="text-xs uppercase tracking-widest hover:opacity-50 transition-opacity">{t('nav.shop')}</Link>
-        <Link href="/category/accessories" className="text-xs uppercase tracking-widest hover:opacity-50 transition-opacity">{t('nav.collections')}</Link>
-        <Link href="/orders" className="text-xs uppercase tracking-widest hover:opacity-50 transition-opacity">Orders</Link>
+        <Link href="/category/clothing" className="text-xs uppercase tracking-widest hover:opacity-50 transition-opacity">
+          {t('nav.shop')}
+        </Link>
+        <Link href="/category/accessories" className="text-xs uppercase tracking-widest hover:opacity-50 transition-opacity">
+          {t('nav.collections')}
+        </Link>
+        <Link href="/orders" className="text-xs uppercase tracking-widest hover:opacity-50 transition-opacity">
+          Orders
+        </Link>
       </div>
 
       <div className="lg:hidden w-10" />
 
       <div className="absolute left-1/2 -translate-x-1/2">
         <Link href="/">
-          <h1 className="text-2xl border-b font-display tracking-[0.3em] font-bold">MARYAM</h1>
+         
+          {isScrolled ? <Image src={Logo} alt="Logo"  width={100} height={100} /> : <Image src={LogoDark} alt="Logo"  width={70} height={70} />}
         </Link>
       </div>
 
@@ -53,7 +67,7 @@ export const Navbar = () => {
           <Heart size={20} strokeWidth={1.5} />
         </Link>
         <button onClick={openCart} className="relative hover:opacity-50 transition-opacity p-2">
-          <ShoppingBag size={20} strokeWidth={1.5} />
+          <ShoppingBag size={20} strokeWidth={1.5}  className='lg:block hidden' />
           {cartItems.length > 0 && (
             <span className="absolute top-1 right-1 bg-brand-gold text-brand-black text-[8px] w-4 h-4 rounded-full flex items-center justify-center font-bold">
               {cartItems.length}
